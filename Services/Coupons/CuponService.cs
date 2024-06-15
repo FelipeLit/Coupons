aa4dccc817f5ed5b6b664abfb9b89a11d9197321
davidmachadoPY <avidmachado@gmail.com>
2024-06-14 20:06:05 -0500

    GET ALL COUPONS

--- Services/Coupons/CuponService.cs
+++ Services/Coupons/CuponService.cs
@@ -1,7 +1,29 @@
+using Coupons.Data;
+using Coupons.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace Coupons
 {
-    public class CuponService : ICuponService
+    // Implementation of ICouponService interface for managing coupons
+    public class CouponService : ICouponService
     {
-        
+        // Private variable to hold the database context
+        private readonly CouponsContext _context;
+
+        // Constructor injecting the database context dependency
+        public CouponService(CouponsContext context)
+        {
+            _context = context;
+        }
+
+        // Asynchronous method to retrieve all coupons
+        public async Task<ICollection<CouponEntity>> GetAllCoupons()
+        {
+            // Returns a list of all coupons from the database
+            return await _context.Coupons.ToListAsync();
+        }
+
     }
 }
