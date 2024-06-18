d7c67680aa262fac932160d8e3ed9d8caae88c37
FelipeGallego <gallegofelipe29@gmail.com>
2024-06-18 15:08:13 +1200

    merge

--- Services/MarketplaceUsers/IMarketplaceUserService.cs
+++ Services/MarketplaceUsers/IMarketplaceUserService.cs
@@ -1,7 +1,16 @@
+using Coupons.Dto;
+using Coupons.Models;
+
 namespace Coupons.Services.MarketplaceUsers
 {
     public interface IMarketplaceUserService
     {
-         
+        // Asynchronous method that returns a task completed with the created marketplace entity.
+        Task<MarketplaceUserEntity> CreateMarketplaceUser(MarketplaceUserDto marketplaceUserDtoDto);
+        //Change status of marketplace Active to Inactive
+        Task<MarketplaceUserEntity> ChangeStatus(int id);
+        //Restore status of marketplace Inactive to active
+        Task<MarketplaceUserEntity> RestoreStatus(int id);
+        Task<ICollection<MarketplaceUserEntity>> GetAllMarketplaceRemove();
     }
 }
